e51500567038031eb85fa4b7fe061587b8fc6ef9
Vijay K <sharmavijay86@gmail.com>
2023-12-02 21:32:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ module "mymod" {
   count = 2
   source  = "app.terraform.io/mevijays/testmod/aws"
   version = "1.0.0"
-  ec2_type = "t2.micro"
-  ec2_tags = {Name = "myapp${count.index}", environment = "development"}
+  instance_type = "t2.micro"
+  instance_tags = {Name = "myapp${count.index}", environment = "development"}
 }
 output "public_ip" {
   value = module.mymod[*].instance_public_ip
